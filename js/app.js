5f01d050f426d54964cc149808b3a67d318e2a79
KhaledAlqrainy <khaledalqrainy@gmail.com>
2021-05-31 01:26:39 +0300

    add seattle

---
+++ js/app.js
@@ -0,0 +1,29 @@
+'use strict';
+
+function randomValue(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+
+const seattle = {
+
+    mincust = 23,
+    maxcust = 65,
+    avgcookie = 6.3,
+    arr:[],
+     randomnum: function(){
+         this.arr.push( math.floor(math.random()*(this.mincust - this.maxcust +1))+this.mincust);
+       console.log(this.arr);  
+     }
+    }
+    seattle.randomnum();
+    
+
+   
+
+
+
+    
+
+
+
